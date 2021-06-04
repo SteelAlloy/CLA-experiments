6e06e6dcba0c65315895bb3ffc82b1534cba5ac6
oganexon <oganexon@protonmail.com>
2021-06-04 11:55:53 +0200

    inputs test

--- src/cla/request-signature.ts
+++ src/cla/request-signature.ts
@@ -3,7 +3,8 @@ export function requestCLASignature() {
     // tbd - e.g. using https://github.com/marketplace/actions/cla-assistant-lite
     // CLAAssistant.requestCLASignature()
 
-    console.log(`hello akshay - ${Deno.args}`)
+    console.log(`hello akshay `)
+    console.log(Deno.args)
 }
 
 requestCLASignature()
