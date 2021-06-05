b3fba282d41a99c639b24f748d7af1a5e1fe2acc
oganexon <oganexon@protonmail.com>
2021-06-05 11:10:00 +0200

    esm.sh core

--- src/cla/mod.ts
+++ src/cla/mod.ts
@@ -1,10 +1,12 @@
 import * as utils from "./utils.ts";
+import { core } from "../deps.ts";
 
 function main(): void {
   console.log("Contributor Assistant: CLA");
-  utils.warning("warning test");
-  utils.error("error test");
-  utils.info("info test");
+  core.warning("warning test");
+  core.error("error test");
+  core.info("info test");
+  core.setFailed("fail test")
 }
 
 main();
--- src/deps.ts
+++ src/deps.ts
@@ -3,4 +3,5 @@ export { parse as parseFlags } from "https://deno.land/std@0.97.0/flags/mod.ts";
 export { format as formatTime } from "https://deno.land/std@0.97.0/datetime/mod.ts";
 
 /* octokit */
-export { Octokit } from "https://cdn.skypack.dev/@octokit/core@3.4.0?dts";
+export { Octokit } from "https://esm.sh/@octokit/core@3.4.0";
+export * as core from "https://esm.sh/@actions/core@1.3.0";
