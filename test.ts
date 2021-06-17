86a547a7d2cb0f339589794c3b25513e9c65dba6
oganexon <oganexon@protonmail.com>
2021-06-17 17:58:10 +0200

    graphql

--- test.ts
+++ test.ts
@@ -1,11 +1,21 @@
-import { parseFlags } from "./src/deps.ts";
+import { authorsQuery } from "./src/cla-functions/meta/graphql.ts";
+import { Octokit } from "./src/utils.ts";
 
-const flags = parseFlags(Deno.args, {
-    string: ["foo"],
-    boolean: "bar",
-    default: {
-        "bar": false
-    }
-});
+const octokit = new Octokit({ auth: "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" });
 
-console.log(flags);
+const variables = {
+  owner: "MaelAcier",
+  name: "Steganography",
+  number: 22,
+};
+
+let commitCursor = "";
+
+const response = await octokit.graphql(
+  authorsQuery,
+  { ...variables, commitCursor },
+);
+
+console.log(authorsQuery)
+
+console.log(response);
