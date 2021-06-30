0a37d7e421bc2806ed5448ab47342ec3db59dee1
oganexon <oganexon@protonmail.com>
2021-06-30 11:13:54 +0200

    payload

--- payload.ts
+++ payload.ts
@@ -1,17 +1,7 @@
 import { context, octokit, personalOctokit, initOctokit } from "./src/utils.ts";
 
-// console.log(context.eventName);
-
 console.log(Deno.inspect(context));
 
-console.log(context.repo)
-
 initOctokit(Deno.env.get("GITHUB_TOKEN") ?? "", "")
-
-await octokit.issues.createLabel({
-    ...context.repo,
-    name: "CLA",
-    color: "1DC515",
-})
 
 // console.log(Deno.inspect(context.payload.issue));
