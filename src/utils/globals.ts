2682264bd01b4ed1d415c509350373327d4102e0
oganexon <oganexon@protonmail.com>
2021-07-01 11:03:15 +0200

    fix

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,5 +1,12 @@
 declare global {
-    const DENO_ENV: string | undefined;
+  const DENO_ENV: string | undefined;
 }
+
+Object.defineProperty(globalThis, "DENO_ENV", {
+  value: "development",
+  writable: false,
+  enumerable: false,
+  configurable: false,
+});
 
 export {};
